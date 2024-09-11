e99b21ac029812216f7dfb359c5505a4a2eb45e1
iamshark31 <farazsyed261@gmail.com>
2024-09-12 00:26:12 +0530

    Fixed Bugs

--- ATM_Simulator_System/src/ASimulatorSystem/Signup2.java
+++ ATM_Simulator_System/src/ASimulatorSystem/Signup2.java
@@ -1,20 +1,22 @@
-
 package ASimulatorSystem;
 
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
-import java.sql.*;
 
-public class Signup2 extends JFrame implements ActionListener{
+public class Signup2 extends JFrame implements ActionListener {
     
-    JLabel l1,l2,l3,l4,l5,l6,l7,l8,l9,l10,l11,l12,l13;
+    JLabel l1, l2, l3, l4, l5, l6, l7, l8, l9, l10, l11, l12, l13;
     JButton b;
-    JRadioButton r1,r2,r3,r4;
-    JTextField t1,t2,t3;
-    JComboBox c1,c2,c3,c4,c5;
+    JRadioButton r1, r2, r3, r4;
+    ButtonGroup group1, group2; // Add ButtonGroup for radio buttons
+    JTextField t1, t2;
+    JComboBox<String> c1, c2, c3, c4, c5;
     String formno;
-    Signup2(String formno){
+    
+    Signup2(String formno) {
+        this.formno = formno;
+        setTitle("NEW ACCOUNT APPLICATION FORM - PAGE 2");
         
         ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("ASimulatorSystem/icons/logo.jpg"));
         Image i2 = i1.getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT);
@@ -22,12 +24,9 @@ public class Signup2 extends JFrame implements ActionListener{
         JLabel l14 = new JLabel(i3);
         l14.setBounds(150, 0, 100, 100);
         add(l14);
-        
-        
-        this.formno = formno;
-        setTitle("NEW ACCOUNT APPLICATION FORM - PAGE 2");
-        
-        l1 = new JLabel("Page 2: Additonal Details");
+
+        // Labels and Form fields setup
+        l1 = new JLabel("Page 2: Additional Details");
         l1.setFont(new Font("Raleway", Font.BOLD, 22));
         
         l2 = new JLabel("Religion:");
@@ -93,168 +92,168 @@ public class Signup2 extends JFrame implements ActionListener{
         r4.setFont(new Font("Raleway", Font.BOLD, 14));
         r4.setBackground(Color.WHITE);
         
+        // Grouping radio buttons
+        group1 = new ButtonGroup();
+        group1.add(r1);
+        group1.add(r2);
         
-        String religion[] = {"Hindu","Muslim","Sikh","Christian","Other"};
-        c1 = new JComboBox(religion);
+        group2 = new ButtonGroup();
+        group2.add(r3);
+        group2.add(r4);
+        
+        String religion[] = {"Hindu", "Muslim", "Sikh", "Christian", "Other"};
+        c1 = new JComboBox<>(religion);
         c1.setBackground(Color.WHITE);
         c1.setFont(new Font("Raleway", Font.BOLD, 14));
         
-        String category[] = {"General","OBC","SC","ST","Other"};
-        c2 = new JComboBox(category);
+        String category[] = {"General", "OBC", "SC", "ST", "Other"};
+        c2 = new JComboBox<>(category);
         c2.setBackground(Color.WHITE);
         c2.setFont(new Font("Raleway", Font.BOLD, 14));
         
-        String income[] = {"Null","<1,50,000","<2,50,000","<5,00,000","Upto 10,00,000","Above 10,00,000"};
-        c3 = new JComboBox(income);
+        String income[] = {"Null", "<1,50,000", "<2,50,000", "<5,00,000", "Upto 10,00,000", "Above 10,00,000"};
+        c3 = new JComboBox<>(income);
         c3.setBackground(Color.WHITE);
         c3.setFont(new Font("Raleway", Font.BOLD, 14));
         
-        String education[] = {"Non-Graduate","Graduate","Post-Graduate","Doctrate","Others"};
-        c4 = new JComboBox(education);
+        String education[] = {"Non-Graduate", "Graduate", "Post-Graduate", "Doctorate", "Others"};
+        c4 = new JComboBox<>(education);
         c4.setBackground(Color.WHITE);
         c4.setFont(new Font("Raleway", Font.BOLD, 14));
         
-        String occupation[] = {"Salaried","Self-Employmed","Business","Student","Retired","Others"};
-        c5 = new JComboBox(occupation);
+        String occupation[] = {"Salaried", "Self-Employed", "Business", "Student", "Retired", "Others"};
+        c5 = new JComboBox<>(occupation);
         c5.setBackground(Color.WHITE);
         c5.setFont(new Font("Raleway", Font.BOLD, 14));
-       
         
+        // Layout setup
         setLayout(null);
         
-        
-        l12.setBounds(700,10,60,30);
+        l12.setBounds(700, 10, 60, 30);
         add(l12);
         
-        l13.setBounds(760,10,60,30);
+        l13.setBounds(760, 10, 60, 30);
         add(l13);
         
-        l1.setBounds(280,30,600,40);
+        l1.setBounds(280, 30, 600, 40);
         add(l1);
         
-        l2.setBounds(100,120,100,30);
+        l2.setBounds(100, 120, 100, 30);
         add(l2);
         
-        c1.setBounds(350,120,320,30);
+        c1.setBounds(350, 120, 320, 30);
         add(c1);
         
-        l3.setBounds(100,170,100,30);
+        l3.setBounds(100, 170, 100, 30);
         add(l3);
         
-        c2.setBounds(350,170,320,30);
+        c2.setBounds(350, 170, 320, 30);
         add(c2);
         
-        l4.setBounds(100,220,100,30);
+        l4.setBounds(100, 220, 100, 30);
         add(l4);
         
-        c3.setBounds(350,220,320,30);
+        c3.setBounds(350, 220, 320, 30);
         add(c3);
         
-        l5.setBounds(100,270,150,30);
+        l5.setBounds(100, 270, 150, 30);
         add(l5);
         
-        c4.setBounds(350,270,320,30);
+        c4.setBounds(350, 270, 320, 30);
         add(c4);
         
-        l11.setBounds(100,290,150,30);
+        l11.setBounds(100, 290, 150, 30);
         add(l11);
         
-        l6.setBounds(100,340,150,30);
+        l6.setBounds(100, 340, 150, 30);
         add(l6);
         
-        c5.setBounds(350,340,320,30);
+        c5.setBounds(350, 340, 320, 30);
         add(c5);
         
-        l7.setBounds(100,390,150,30);
+        l7.setBounds(100, 390, 150, 30);
         add(l7);
         
-        t1.setBounds(350,390,320,30);
+        t1.setBounds(350, 390, 320, 30);
         add(t1);
         
-        l8.setBounds(100,440,180,30);
+        l8.setBounds(100, 440, 180, 30);
         add(l8);
         
-        t2.setBounds(350,440,320,30);
+        t2.setBounds(350, 440, 320, 30);
         add(t2);
         
-        l9.setBounds(100,490,150,30);
+        l9.setBounds(100, 490, 150, 30);
         add(l9);
         
-        r1.setBounds(350,490,100,30);
+        r1.setBounds(350, 490, 100, 30);
         add(r1);
         
-        r2.setBounds(460,490,100,30);
+        r2.setBounds(460, 490, 100, 30);
         add(r2);
         
-        l10.setBounds(100,540,180,30);
+        l10.setBounds(100, 540, 180, 30);
         add(l10);
         
-        r3.setBounds(350,540,100,30);
+        r3.setBounds(350, 540, 100, 30);
         add(r3);
         
-        r4.setBounds(460,540,100,30);
+        r4.setBounds(460, 540, 100, 30);
         add(r4);
         
-        b.setBounds(570,640,100,30);
+        b.setBounds(570, 640, 100, 30);
         add(b);
         
         b.addActionListener(this);
         
         getContentPane().setBackground(Color.WHITE);
         
-        setSize(850,750);
-        setLocation(500,120);
+        setSize(850, 750);
+        setLocation(500, 120);
         setVisible(true);
     }
     
-    public void actionPerformed(ActionEvent ae){
-        String religion = (String)c1.getSelectedItem(); 
-        String category = (String)c2.getSelectedItem();
-        String income = (String)c3.getSelectedItem();
-        String education = (String)c4.getSelectedItem();
-        String occupation = (String)c5.getSelectedItem();
+    public void actionPerformed(ActionEvent ae) {
+        String religion = (String) c1.getSelectedItem(); 
+        String category = (String) c2.getSelectedItem();
+        String income = (String) c3.getSelectedItem();
+        String education = (String) c4.getSelectedItem();
+        String occupation = (String) c5.getSelectedItem();
         
         String pan = t1.getText();
         String aadhar = t2.getText();
         
         String scitizen = "";
-        if(r1.isSelected()){ 
+        if (r1.isSelected()) { 
             scitizen = "Yes";
-        }
-        else if(r2.isSelected()){ 
+        } else if (r2.isSelected()) { 
             scitizen = "No";
         }
            
         String eaccount = "";
-        if(r3.isSelected()){ 
+        if (r3.isSelected()) { 
             eaccount = "Yes";
-        }else if(r4.isSelected()){ 
+        } else if (r4.isSelected()) { 
             eaccount = "No";
         }
         
-        try{
-            if(t2.getText().equals("")){
+        try {
+            if (t1.getText().equals("")) {
                 JOptionPane.showMessageDialog(null, "Fill all the required fields");
-            }else{
+            } else {
                 Conn c1 = new Conn();
-                String q1 = "insert into signup2 values('"+formno+"','"+religion+"','"+category+"','"+income+"','"+education+"','"+occupation+"','"+pan+"','"+aadhar+"','"+scitizen+"','"+eaccount+"')";
-                c1.s.executeUpdate(q1);
+                String query = "INSERT INTO signup2 VALUES('" + formno + "','" + religion + "','" + category + "','" + income + "','" + education + "','" + occupation + "','" + pan + "','" + aadhar + "','" + scitizen + "','" + eaccount + "')";
+                c1.s.executeUpdate(query);
                 
                 new Signup3(formno).setVisible(true);
                 setVisible(false);
             }
-                
-      
-            
-        }catch(Exception ex){
-             ex.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-    
-               
     }
     
-    
-    public static void main(String[] args){
+    public static void main(String[] args) {
         new Signup2("").setVisible(true);
     }
 }
